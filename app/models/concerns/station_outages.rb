a60393ab2c61bf9d9cd352378425a0e54383c0eb
Matt Schmaus <mschmaus201@gmail.com>
2015-01-11 19:10:45 -0500

    Add StationOutages module

---
+++ app/models/concerns/station_outages.rb
@@ -0,0 +1,10 @@
+module StationOutages
+  def self.get_outages
+    xml_feed = HTTParty.get('http://web.mta.info/developers/data/nyct/nyct_ene.xml')
+    outages = xml_feed['NYCOutages']['outage']
+
+    outages.each do |outage|
+      station_routes = outage['trainno'].match(/(\w{1})\//)
+    end
+  end
+end
