f8ed175492818a600842854d1730df1daad5d4cc
Matt Schmaus <mschmaus201@gmail.com>
2015-01-21 23:37:18 -0500

    Add lat_long and served_routes helper methods on Station

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,11 @@
 class Station < ActiveRecord::Base
   has_many :entrances
+
+  def lat_long
+    "#{latitude}, #{longitude}"
+  end
+
+  def served_routes
+    entrances.map(&:routes).uniq.flatten.sort
+  end
 end
